2fce1e1dbd0a0a659f31a7e3f2a316b62f547adf
Nikhil Ranjan <nikhil.ranjan@infoedge.com>
2020-08-17 14:28:29 +0530

    Updates

---
+++ precache-manifest.ae1c01751159088c8c4105df3931891a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9948af7bcf84d7d322455d31f1636e93",
+    "url": "/bluestackCampaignManager/index.html"
+  },
+  {
+    "revision": "ab7290217829d04d9463",
+    "url": "/bluestackCampaignManager/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "0b4b8b8bc72c3e9f6b3c",
+    "url": "/bluestackCampaignManager/static/css/main.5a765d90.chunk.css"
+  },
+  {
+    "revision": "ab7290217829d04d9463",
+    "url": "/bluestackCampaignManager/static/js/2.b222bd7d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/bluestackCampaignManager/static/js/2.b222bd7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4b8b8bc72c3e9f6b3c",
+    "url": "/bluestackCampaignManager/static/js/main.f598f72d.chunk.js"
+  },
+  {
+    "revision": "c97890ca3e70ece828b2",
+    "url": "/bluestackCampaignManager/static/js/runtime-main.d9387b5a.js"
+  }
+]);
